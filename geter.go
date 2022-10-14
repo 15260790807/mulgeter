01c352f4fe6387891f1008c0c14d1753df0c5ee3
zjq <179470917@qq.com>
2022-10-14 09:19:50 +0800

    fix: 根据分片配置

--- geter.go
+++ geter.go
@@ -3,23 +3,38 @@ package mulgeter
 import (
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
+const FENPIAN = 2048
+
+type TestInterface interface {
+	Read()
+}
 type Mulgeter struct {
 	length     int
 	tasknums   int
 	overnums   int
+	filename   string
+	fileext    string
 	requestUrl string
+	TestInterface
 }
 
 func NewMulgeter(url string) *Mulgeter {
+	urlslice := strings.Split(url, "/")
+	fmt.Printf("%v", urlslice)
+	filename := urlslice[len(urlslice)-1]
+	fmt.Printf("名字%s", filename)
 	return &Mulgeter{
 		requestUrl: url,
+		filename:   filename,
 	}
 }
 
@@ -32,13 +47,50 @@ func (m *Mulgeter) GetLength() int {
 func (m *Mulgeter) Length() int {
 	return m.length
 }
+func (m *Mulgeter) Read() {
+	fmt.Println("读取")
+}
+func (m *Mulgeter) CalcTask() {
+	c := float64(m.length) / FENPIAN
+	tx := math.Ceil(c)
+	fmt.Print(tx)
+	m.tasknums = int(int64(tx))
+	fmt.Println("任务数", m.tasknums)
+	return
+}
 func (m *Mulgeter) BeginDownload() {
+	m.Read()
+	m.CalcTask()
 	var wg sync.WaitGroup
-	for i := 0; i < m.length; i++ {
+	startt := 0
+	endt := 0
+	lett := m.length
+	for i := 0; i < m.tasknums; i++ {
 		j := i
+		fmt.Println("任务", j)
+		if m.length <= FENPIAN {
+			startt = 0
+			endt = m.length - 1
+		} else {
+			if j == 0 {
+				startt = 0
+				endt = FENPIAN - 1
+			} else {
+				startt = endt + 1
+				if lett <= FENPIAN {
+					endt = startt + lett - 1
+				} else {
+					endt = startt + FENPIAN - 1
+				}
+			}
+			lett = lett - FENPIAN
+			fmt.Println("lett", lett)
+			fmt.Println("lett", startt)
+		}
 		wg.Add(1)
 		go func() {
-			str := fmt.Sprintf("bytes=%d-%d", j, j)
+			str := fmt.Sprintf("bytes=%d-%d", startt, endt)
+			fmt.Println(str)
 			reqObj, err := http.NewRequest("GET", m.requestUrl, nil)
 			if err != nil {
 				fmt.Println("错误")
@@ -57,7 +109,7 @@ func (m *Mulgeter) BeginDownload() {
 			}()
 			// b, err := ReadAll(repon.Body)
 			// 有多少
-			f, err := os.OpenFile(fmt.Sprintf("%d.txt", j), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+			f, err := os.OpenFile(fmt.Sprintf("%d", j), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 			b, err := io.Copy(f, resp.Body)
 			if err != nil {
 				return
@@ -71,9 +123,9 @@ func (m *Mulgeter) BeginDownload() {
 }
 func (m *Mulgeter) mergeFile() {
 	fmt.Println("下载完成")
-	zongfile, _ := os.OpenFile("test.txt", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	for i := 0; i < m.length; i++ {
-		f, _ := os.OpenFile(fmt.Sprintf("%d.txt", i), os.O_RDWR, 0666)
+	zongfile, _ := os.OpenFile(m.filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	for i := 0; i < m.tasknums; i++ {
+		f, _ := os.OpenFile(fmt.Sprintf("%d", i), os.O_RDWR, 0666)
 		wrted, _ := io.Copy(zongfile, f)
 		fmt.Println("写入多少字节", wrted)
 	}
